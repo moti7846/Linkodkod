2fd021468a7d7de0093bf718725d9723a7ab6b98
moti7846 <8563816@gmail.com>
2025-09-08 12:42:36 +0300

    update route comments

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -2,19 +2,19 @@ import express from 'express';
 
 const router = express.Router();
 
-// GET /posts -> allposts 
+// GET /posts/all-posts -> allposts 
 router.get("/all-posts", (req, res) => {return res.json({url : req.url})} )
 
-// GET /posts/:id -> post by id
+// GET /posts/post:id -> post by id
 router.get("/post:id", (req, res) => {return res.json({url : req.url, id: req.params.id})})
 
-// // POST /posts -> create new post
+// // POST /posts/create -> create new post
 // router.post("/create", )
 
-// // PUT /posts/:id -> updata/edit post
-// router.put("/updata:id", )
+// // PUT /posts/update:id -> update/edit post
+// router.put("/update:id", )
 
-// // DELETE /posts/:id -> delete post
+// // DELETE /posts/delete:id -> delete post
 // router.delete("/delete:id",)
 
 
