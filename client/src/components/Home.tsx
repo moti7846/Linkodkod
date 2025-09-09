d2a783c4160f2d89d5958eb9bccc3fa5e6ac0d95
moti7846 <8563816@gmail.com>
2025-09-09 11:17:43 +0300

    End of page 1

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,21 +3,25 @@ import Post from "./Post"
 import "./styles/Home.css"
 
 export default function Home() {
-    const [posts, setPost] : [Post[], React.Dispatch<React.SetStateAction<[]>>] = useState([])
-    const [err, setErr] = useState(false)
+    const [posts, setPost] : [Post[], React.Dispatch<React.SetStateAction<[]>>] = useState([]);
+    const [isError, setIsError] : [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(false);
+    const [isLoader, setIsLoader] : [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(!isError);
 
     function getPosts() {
         fetch("http://localhost:3200/posts/all-posts")
             .then(response => {
                 if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    throw new Error(`HTTP isError! status: ${response.status}`);
                 }
                 return response.json();
             }).then(data => {
-                setPost(data);
+                setTimeout(() => {
+                    setIsLoader((p) => !p)
+                    setPost(data);
+                }, 5000);
             })
             .catch(() => {
-                setErr(true)
+                setIsError(true)
             });
 
     }
@@ -27,9 +31,17 @@ export default function Home() {
     }, [])
 
     return (
-        <div className="grid-cards home">
-            {posts.map((p) => (<Post urlToImg={`http://localhost:3200/${p.id}.png`} description={p.description} likes={p.likes} namePost={p.name} />))}
-            {err && <h2>Error fetching data</h2>}
+        <div className={`home ${!isError && !isLoader ? 'grid-cards' : 'error'}`}>
+            {
+                isLoader && <span className="loader"></span>
+            }
+            {
+                !isError && posts.map((p) => (<Post urlToImg={`http://localhost:3200/${p.id}.png`} description={p.description} likes={p.likes} namePost={p.name} />))
+            }
+
+            {
+                isError && <h2>Error fetching data</h2>
+            }
         </div>
     )
 }
