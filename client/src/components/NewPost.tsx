973a335c276c7b4ed7af5ed3b05eed1c6dbb3ae7
moti7846 <8563816@gmail.com>
2025-09-10 15:26:37 +0300

    finish css new post

--- client/src/components/NewPost.tsx
+++ client/src/components/NewPost.tsx
@@ -1,32 +1,42 @@
+import { useState } from "react";
 import "./styles/NewPost.css"
 
 export default function NewPost() {
+    const [msg, setMsg] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
     async function formNewPost(e : any) {
+        setIsLoading(true)
         e.preventDefault();
-        const [name, description, img] = [e.target.name.value, e.target.description.value, e.target.img.value]
+        const [name, description, likes] = [e.target.name.value, e.target.description.value, e.target.likes.value]
         console.log(name)
         console.log(description)
-        console.log(img)
+        console.log(likes)
         const response = await fetch("http://localhost:3200/posts/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({ name, description, time : new Date().toLocaleString() , img })
+        body: JSON.stringify({ name, description, time : new Date().toLocaleString() , likes })
       })
-      console.log(response)
+      setTimeout(() => {
+        setIsLoading(false)
+        setMsg(response.statusText)
+      }, 3000);
     }
   return (
     <div className="NewPost">
         <form onSubmit={formNewPost}>
             <h2>create new post</h2>
             <label htmlFor="name">name</label>
-            <input type="text" minLength={5} maxLength={20} id="name" />
+            <input type="text" required maxLength={20} id="name" />
             <label htmlFor="description">description</label>
-            <input type="text" id="description" />
-            <label htmlFor="img">image</label>
-            <input type="file" accept="image/png" id="img" />
+            <input type="text" required maxLength={240} id="description" />
+            <label htmlFor="likes">likes</label>
+            <input type="number" min={0} id="likes" />
             <button type="submit">Create Post</button>
+        { msg && <h3 className="msg">{msg}</h3> }
+        { isLoading && <span className="loaderNewPost"></span> }
         </form>
     </div>
   )
